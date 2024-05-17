028d51f96f7c4f7d2907ee15aa77d2379e0d6f3b
Gabriel Munera <gabrielmumo@MacBook-Pro-de-Gabriel.local>
2024-05-16 21:15:41 -0500

    User & Role repositories

---
+++ src/main/java/dev/gabrielmumo/demo/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package dev.gabrielmumo.demo.repository;
+
+import dev.gabrielmumo.demo.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
--- src/main/java/dev/gabrielmumo/demo/repository/UserRepository.java
+++ src/main/java/dev/gabrielmumo/demo/repository/UserRepository.java
@@ -1,2 +1,12 @@
-package dev.gabrielmumo.demo.repository;public interface UserRepository {
+package dev.gabrielmumo.demo.repository;
+
+import dev.gabrielmumo.demo.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
+
+    Boolean existsByUsername(String username);
 }
