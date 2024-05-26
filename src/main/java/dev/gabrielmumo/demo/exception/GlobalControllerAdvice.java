ad50e418ec7c301584a70e26ea7313d383ed04fe
Gabriel Munera <gabrielmumo@MacBook-Pro-de-Gabriel.local>
2024-05-25 22:23:01 -0500

    Adds global handler for bad request

--- src/main/java/dev/gabrielmumo/demo/exception/GlobalControllerAdvice.java
+++ src/main/java/dev/gabrielmumo/demo/exception/GlobalControllerAdvice.java
@@ -4,6 +4,7 @@ import dev.gabrielmumo.demo.dto.ExceptionDto;
 import jakarta.persistence.EntityNotFoundException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.coyote.BadRequestException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,11 +16,20 @@ public class GlobalControllerAdvice {
     private static final Log LOG = LogFactory.getLog(GlobalControllerAdvice.class);
 
     @ExceptionHandler({EntityNotFoundException.class})
-    public ResponseEntity<ExceptionDto> handleUnauthorizedException(EntityNotFoundException e) {
+    public ResponseEntity<ExceptionDto> handleEntityNotFoundException(EntityNotFoundException e) {
         String message = e.getMessage();
         LOG.error(message, e);
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ExceptionDto.Builder(message, HttpStatus.NOT_FOUND).build());
+    }
+
+    @ExceptionHandler({BadRequestException.class})
+    public ResponseEntity<ExceptionDto> handleBadRequestException(BadRequestException e) {
+        String message = e.getMessage();
+        LOG.error(message, e);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionDto.Builder(message, HttpStatus.BAD_REQUEST).build());
     }
 }
--- src/main/java/dev/gabrielmumo/demo/service/UserServiceImpl.java
+++ src/main/java/dev/gabrielmumo/demo/service/UserServiceImpl.java
@@ -3,6 +3,8 @@ package dev.gabrielmumo.demo.service;
 import dev.gabrielmumo.demo.dto.UserDto;
 import dev.gabrielmumo.demo.repository.UserRepository;
 import dev.gabrielmumo.demo.utils.UserConverter;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,8 @@ import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService{
+
+    private static final Log LOG = LogFactory.getLog(UserServiceImpl.class);
 
     private final UserRepository userRepository;
     private final UserConverter userConverter;
@@ -23,8 +27,11 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public Optional<UserDto> signup(UserDto user) throws Exception {
-        if(userRepository.existsByUsername(user.username()))
-            throw new BadRequestException("Username already exists" + user.username());
+        if(userRepository.existsByUsername(user.username())) {
+            var message = String.format("Username already exists %s", user.username());
+            LOG.error(message);
+            throw new BadRequestException(message);
+        }
         userRepository.save(userConverter.toEntity(user));
         return Optional.of(user);
     }
