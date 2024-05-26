ed06cfec010d343f7c9fabf2f194953850bb483e
Gabriel Munera <gabrielmumo@MacBook-Pro-de-Gabriel.local>
2024-05-25 21:25:37 -0500

    Global exception handler

--- src/main/java/dev/gabrielmumo/demo/exception/GlobalControllerAdvice.java
+++ src/main/java/dev/gabrielmumo/demo/exception/GlobalControllerAdvice.java
@@ -1,6 +1,7 @@
 package dev.gabrielmumo.demo.exception;
 
 import dev.gabrielmumo.demo.dto.ExceptionDto;
+import jakarta.persistence.EntityNotFoundException;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.http.HttpStatus;
@@ -13,12 +14,12 @@ public class GlobalControllerAdvice {
 
     private static final Log LOG = LogFactory.getLog(GlobalControllerAdvice.class);
 
-    @ExceptionHandler({UnauthorizedException.class})
-    public ResponseEntity<ExceptionDto> handleUnauthorizedException(UnauthorizedException e) {
-        String message = "Unauthorized";
+    @ExceptionHandler({EntityNotFoundException.class})
+    public ResponseEntity<ExceptionDto> handleUnauthorizedException(EntityNotFoundException e) {
+        String message = e.getMessage();
         LOG.error(message, e);
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(new ExceptionDto.Builder(message, HttpStatus.UNAUTHORIZED).build());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ExceptionDto.Builder(message, HttpStatus.NOT_FOUND).build());
     }
 }
